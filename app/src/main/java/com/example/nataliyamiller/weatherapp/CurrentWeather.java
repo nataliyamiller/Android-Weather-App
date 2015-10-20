3dbd3bbecaa50532a1b6d7254b3e1f52fac40161
nataliyamiller <nataliyamiller82@gmail.com>
2015-10-20 00:32:37 -0700

    creates formatted time

--- app/src/main/java/com/example/nataliyamiller/weatherapp/CurrentWeather.java
+++ app/src/main/java/com/example/nataliyamiller/weatherapp/CurrentWeather.java
@@ -1,5 +1,9 @@
 package com.example.nataliyamiller.weatherapp;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by nataliyamiller on 10/19/15.
  */
@@ -10,52 +14,106 @@ public class CurrentWeather {
     private double mHumidity;
     private double mPrecipChance;
     private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.mTimeZone = timeZone;
+    }
 
     public String getIcon() {
         return mIcon;
     }
 
-    public void setIcon(String mIcon) {
-        this.mIcon = mIcon;
+    public void setIcon(String icon) {
+        this.mIcon = icon;
+    }
+
+    public int getIconId() {
+        int iconId = R.drawable.clear_day;
+
+        if(mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+        return iconId;
     }
 
     public long getTime() {
         return mTime;
     }
 
-    public void setTime(long mTime) {
-        this.mTime = mTime;
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() * 1000);
+        String timeString = formatter.format(dateTime);
+
+        return timeString;
+    }
+
+    public void setTime(long time) {
+        this.mTime = time;
     }
 
     public double getTemperature() {
         return mTemperature;
     }
 
-    public void setTemperature(double mTemperature) {
-        this.mTemperature = mTemperature;
+    public void setTemperature(double temperature) {
+        this.mTemperature = temperature;
     }
 
     public double getHumidity() {
         return mHumidity;
     }
 
-    public void setHumidity(double mHumidity) {
-        this.mHumidity = mHumidity;
+    public void setHumidity(double humidity) {
+        this.mHumidity = humidity;
     }
 
     public double getPrecipChance() {
         return mPrecipChance;
     }
 
-    public void setPrecipChance(double mPrecipChance) {
-        this.mPrecipChance = mPrecipChance;
+    public void setPrecipChance(double precipChance) {
+        this.mPrecipChance = precipChance;
     }
 
     public String getSummary() {
         return mSummary;
     }
 
-    public void setSummary(String mSummary) {
-        this.mSummary = mSummary;
+    public void setSummary(String summary) {
+        this.mSummary = summary;
     }
 }
